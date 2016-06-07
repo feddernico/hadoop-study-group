8f6dea17c3e250c60cf9f5497e8c45bf9d52b175
feddernico <federico.viscioletti@gmail.com>
2016-06-07 09:40:19 +0100

    Updated rename_books.sh file

--- util/script/rename_books.sh
+++ util/script/rename_books.sh
@@ -11,7 +11,17 @@
 # Usage:
 # ./rename_books.sh /path/to/books/folder/
 
-# path to files to be read from input parameter	
+# moves txt files from sub-folders (if present) to parents
+find $1 -mindepth 2 -type f -print -exec mv {} $1 \; 
+
+# removes just the subfolders from the files folder
+rm -r $1/*/
+
+# removes every file that doesn't have .txt extension
+find $1 -type f ! -iname "*.txt" -delete
+
+# removes all the folders
+
 for f in $1*
 do
 	# logging which file is processing
@@ -21,23 +31,41 @@ do
 	book_title="`grep 'Title:' $f`"  
 	
 	# removes the Title: part
-	book_title_clean=${book_title##*:}
+	book_title_clean=${book_title##*Title:}
 	
 	#if it finds the word Complete in it, it removes the last part from the book name
-	if [[ $book_title_clean == *"Complete"* ]]
+	if [[ $book_title_clean == *"- Complete" ]]
 	then
+		echo book title clean: $book_title_clean
 		book_title_clean=${book_title_clean::-11}
-		file_name=$(echo $book_title_clean | sed -e 's/[^A-Za-z0-9._-]/_/g')
-		file_name_clean=$file_name.txt
-	else 
-		file_name=$(echo $book_title_clean | sed -e 's/[^A-Za-z0-9._-]/_/g')
-		# removes the last underscore before the .txt
-		file_name_clean=${file_name%_*}.txt
 	fi
+
+	# every character that is not a letter or number or a ._- is converted into a _
+	file_name=$(echo $book_title_clean | sed -e 's/[^A-Za-z0-9._-]/_/g')
+
+	file_name=$(echo $file_name | sed -e 's/__/_/g')
 	
+	# if the last character of the file name is a _ removes it
+	if [[ $file_name == *"_" ]]
+	then
+		echo book title clean: $book_title_clean
+		file_name=${file_name::-1}
+	fi
+
+	# gets the file name length
+	file_name_len=${#file_name}
+
+	if [[ $file_name_len > 255 ]]
+	then 
+		file_name=$(echo $file_name | cut -c1-100)
+	fi
+
+	# removes dots in the file
+	file_name_clean=${file_name//./}.txt
+
 	# copies the book file into the new folder called books_clean
 	mkdir -p books_clean
-	cp $f books_clean/$file_name_clean
+	mv $f books_clean/$file_name_clean
 
 	echo "file: $file_name_clean written"
 done
